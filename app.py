21a9a969f3d4952b749bed5b05c22f4c98a62119
XMarulandaH <143102196+XMarulandaH@users.noreply.github.com>
2024-05-27 10:19:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if uploaded_file is not None:
 
     st.write(df1)
     st.subheader('Estadísticos básicos de los sensores.')
-    st.dataframe(df1["temperatura"].describe())
+    st.dataframe(df1["temperatura", "Humedad"].describe())
 
     min_temp = st.slider('Selecciona valor mínimo del filtro ', min_value=-10, max_value=45, value=23, key=1)
     # Filtrar el DataFrame utilizando query
