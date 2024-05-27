26ab0f9056b8afa3b0074cca1d443417bcb13ca0
XMarulandaH <143102196+XMarulandaH@users.noreply.github.com>
2024-05-27 08:55:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,6 @@ if uploaded_file is not None:
 
     st.write(df1)
     st.subheader('Estadísticos básicos de los sensores.')
-    st.dataframe(df1["temperatura ESP32"].describe())
     st.dataframe(df1["Humedad ESP32"].describe())
 
     min_temp = st.slider('Selecciona valor mínimo del filtro ', min_value=-10, max_value=45, value=23, key=1)
