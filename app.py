01870aa6a47a0d69ec3bebef5c4ea8ccfadf49fb
XMarulandaH <143102196+XMarulandaH@users.noreply.github.com>
2024-05-27 12:35:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,9 @@ if uploaded_file is not None:
     st.write('Dataframe Filtrado')
     st.write(filtrado_df_min)
 
-    max_temp = st.slider('Selecciona el valor máximo del filtro ', min_value=-10, max_value=45, value=23, key=2, style={'background-color': 'green'})
+    st.markdown('<style>div[data-baseweb="slider"] { background-color: green; }</style>', unsafe_allow_html=True)
+    
+    max_temp = st.slider('Selecciona el valor máximo del filtro ', min_value=-10, max_value=45, value=23, key=2)
     # Filtrar el DataFrame utilizando query
     filtrado_df_max = df1.query(f"`temperatura` < {max_temp}")
     # Mostrar el DataFrame filtrado
