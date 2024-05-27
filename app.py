733e0548fa5958121bc656514abb973ec1f598dd
XMarulandaH <143102196+XMarulandaH@users.noreply.github.com>
2024-05-27 09:06:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -44,45 +44,32 @@ uploaded_file = st.file_uploader('Choose a file')
 if uploaded_file is not None:
     df1 = pd.read_csv(uploaded_file)
 
-    # Mostrar las columnas del DataFrame
-    st.write("Columnas del archivo CSV:")
-    st.write(df1.columns)
-
     st.subheader('Perfil gráfico de la variable medida.')
     df1 = df1.set_index('Time')
     st.line_chart(df1)
+    st.line_chart(df2)
 
     st.write(df1)
     st.subheader('Estadísticos básicos de los sensores.')
-
-    # Verifica si las columnas existen antes de intentar usarlas
-    if "temperatura ESP32" in df1.columns:
-        st.dataframe(df1["temperatura"].describe())
-    else:
-        st.warning('La columna "temperatura ESP32" no se encontró en el archivo CSV.')
-
-    if "Humedad" in df1.columns:
-        st.dataframe(df1["Humedad"].describe())
-    else:
-        st.warning('La columna "Humedad" no se encontró en el archivo CSV.')
+    st.dataframe(df1["temperatura"].describe())
+    st.write(df2)
+    st.dataframe(df2["Humedad"].describe())
 
     min_temp = st.slider('Selecciona valor mínimo del filtro ', min_value=-10, max_value=45, value=23, key=1)
     # Filtrar el DataFrame utilizando query
-    if "temperatura ESP32" in df1.columns:
-        filtrado_df_min = df1.query(f"`temperatura ESP32` > {min_temp}")
-        # Mostrar el DataFrame filtrado
-        st.subheader("Temperaturas superiores al valor configurado.")
-        st.write('Dataframe Filtrado')
-        st.write(filtrado_df_min)
+    filtrado_df_min = df1.query(f"`temperatura ESP32` > {min_temp}")
+    # Mostrar el DataFrame filtrado
+    st.subheader("Temperaturas superiores al valor configurado.")
+    st.write('Dataframe Filtrado')
+    st.write(filtrado_df_min)
 
     max_temp = st.slider('Selecciona valor máximo del filtro ', min_value=-10, max_value=45, value=23, key=2)
     # Filtrar el DataFrame utilizando query
-    if "temperatura ESP32" in df1.columns:
-        filtrado_df_max = df1.query(f"`temperatura ESP32` < {max_temp}")
-        # Mostrar el DataFrame filtrado
-        st.subheader("Temperaturas Inferiores al valor configurado.")
-        st.write('Dataframe Filtrado')
-        st.write(filtrado_df_max)
+    filtrado_df_max = df1.query(f"`temperatura ESP32` < {max_temp}")
+    # Mostrar el DataFrame filtrado
+    st.subheader("Temperaturas Inferiores al valor configurado.")
+    st.write('Dataframe Filtrado')
+    st.write(filtrado_df_max)
 
 else:
     st.warning('Necesitas cargar un archivo csv excel.')
