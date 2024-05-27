04f677410a3ccadbc8832cdaa8a73157cf2c1d94
XMarulandaH <143102196+XMarulandaH@users.noreply.github.com>
2024-05-27 12:38:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,6 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-st.markdown(
-    """
-    <style>
-    .streamlit-slider > div {
-        background: linear-gradient(to right, green, blue) !important;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 st.markdown('<h2 style="color: green;text-align: center;">Análisis de temperatura y humedad de mi compostador</h2>', unsafe_allow_html=True)
 image = Image.open('images2.jpg')
 st.image(image)
